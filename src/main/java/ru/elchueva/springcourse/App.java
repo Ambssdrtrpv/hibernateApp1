89605fc612c549575066ab37677abc9920192f5c
”Sabina <sabina.elchueva@gmail.com>
2024-08-09 13:14:27 +0300

    change order and person

--- src/main/java/ru/elchueva/springcourse/App.java
+++ src/main/java/ru/elchueva/springcourse/App.java
@@ -22,12 +22,13 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get( Person.class, 3);
-            List<Item> items = person.getItems();
-            for (Item item : items) {
-                session.remove(item);
-            }
-            person.getItems().clear();
+           Person person = session.get(Person.class, 4);
+           Item item = session.get(Item.class, 1);
+
+           item.getOwner().getItems().remove(item);
+           item.setOwner(person);
+           person.getItems().add(item);
+
 
             session.getTransaction().commit(); //делает все необходимые sql запросы
         } finally {
